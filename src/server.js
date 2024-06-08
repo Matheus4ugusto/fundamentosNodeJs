167387a1e12422ffd1a8209ce2ab464e55db00d0
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-06-08 20:18:19 -0300

    Aula 16 - Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 import {randomUUID} from 'node:crypto'
 import {json} from "./middlewares/json.js";
 import {Database} from "./database.js";
+import {routes} from "./routes.js";
 // adicionar 'type': '[module]' no package.json para poder usar o modelo import e não require
 // utilizar o prefixo 'node:' antes de importar módulos internos do node
 
@@ -26,7 +27,6 @@ import {Database} from "./database.js";
 
 // Cabeçalhos(req/res) => Metadados
 
-const database = new Database()
 
 const server = http.createServer(async (req,
                                         res) => {
@@ -34,26 +34,12 @@ const server = http.createServer(async (req,
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-        const users = database.select('users')
-
-        return res.end(JSON.stringify(users)) //Early return
-    }
-
-    if (method === 'POST' && url === '/users') {
-
-        const {name, email} = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
